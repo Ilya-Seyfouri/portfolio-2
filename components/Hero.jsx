a989f2f22d70b9d9c7c828cf608fe579c45dd14e
iseyf <c4033962@newcastle.ac.uk>
2025-11-12 18:39:31 +0000

    xd

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,13 +11,13 @@ export default function Hero(){
           <div className="container mx-auto">
             <div className="pt-35">
               <div className="flex justify-center items-center">
-                <h2 className="text-5xl md:text-7xl">Hi, I'm Ilya</h2>
+                <h2 className="text-4xl md:text-7xl">Hi, I'm Ilya</h2>
               </div>
               <div className="pt-6 flex justify-center items-center">
-                <h2 className="text-5xl md:text-7xl">software engineer.</h2>
+                <h2 className="text-4xl md:text-7xl items-center">software engineer.</h2>
               </div>
               <div className="pt-10 flex justify-center items-center">
-                <p className="text-xl md:text-2xl">computer science & ai @ newcastle university</p>
+                <p className="text-lg md:text-2xl items-center">computer science & ai @ newcastle university</p>
               </div>
 
               <div className="pt-14 md:pt-10 flex justify-center items-center md:pb-30">
